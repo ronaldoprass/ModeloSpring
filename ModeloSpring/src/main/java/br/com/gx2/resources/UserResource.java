745368117427da450b5fd1211ff1af0ce12d7244
rprass <rprass@gmail.com>
2020-01-29 19:11:15 -0200

    Adição do Profile de Testes;

--- ModeloSpring/src/main/java/br/com/gx2/resources/UserResource.java
+++ ModeloSpring/src/main/java/br/com/gx2/resources/UserResource.java
@@ -1,21 +1,36 @@
 package br.com.gx2.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.gx2.entities.User;
+import br.com.gx2.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
-	@GetMapping
-	public ResponseEntity<User> findAll() {
+	@Autowired
+	private UserService service;
 
-		User user = new User(1, "Ronaldo Prass", "rprass@gmail.com", "6666", "666");
-		return ResponseEntity.ok(user);
+	@GetMapping
+	public ResponseEntity<List<User>> findAll() {
+
+		List<User> users = service.findAll();
+		return ResponseEntity.ok(users);
+	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Integer id) {
+		User obj = service.findByID(id);
+
+		return ResponseEntity.ok(obj);
 	}
 
 }
